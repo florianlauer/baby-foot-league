68e8711d62c3d93c5f7668a9a60c50a7b8b20d94
florianlauer <florian@sencrop.com>
2024-10-14 22:20:04 +0200

    feat(responsive): reponsive views

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { List, LogOut, Trophy, Users } from "lucide-react";
 
-const NewHeader = ({ onLogout }: { onLogout: VoidFunction }) => {
+const Header = ({ onLogout }: { onLogout: VoidFunction }) => {
   return (
-    <header className="bg-green-700 text-white shadow-md">
+    <header className="bg-green-700 text-white shadow-md fixed top-0 left-0 right-0 h-16 z-10 overflow-hidden">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <Link to="/" className="flex items-center space-x-2">
           <Trophy size={32} />
           <span className="text-xl font-bold">Baby Foot League</span>
         </Link>
-        <nav>
+        <nav className="hidden md:block">
           <ul className="flex space-x-4">
             <li>
               <Link
@@ -53,4 +53,4 @@ const NewHeader = ({ onLogout }: { onLogout: VoidFunction }) => {
   );
 };
 
-export default NewHeader;
+export default Header;
